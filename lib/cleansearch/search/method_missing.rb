ed9b8d8cc0f64ed1a7e3fab909bbb5114f318a6c
Rafael Lima <rafa.cbl@gmail.com>
2011-11-30 12:28:44 -0500

    Caching calls to method_missing and testing the Matcher class.

--- lib/cleansearch/search/method_missing.rb
+++ lib/cleansearch/search/method_missing.rb
@@ -3,25 +3,37 @@ module CleanSearch
       module MethodMissing
       
         def find(conditions = {})
-          
+          #to do:
         end
       
-        def method_missing(method_sym, *arguments, &block)
+        def self.method_missing(method_sym, *arguments, &block)
           match = Matcher.new(method_sym)
           if match.match?
-            find(match.attribute => arguments.first)
+            dynamic_finder(method_sym, match.attribute)
+            send(method_sym, arguments.first)
           else
             super
           end
         end
         
-        def respond_to?(method_sym, include_private = false)
+        def self.respond_to?(method_sym, include_private = false)
           if Matcher.new(method_sym).match?
             true
           else
             super
           end
         end
+        
+        protected
+        
+        #caching calls to method_missing
+        def self.define_dynamic_finder(finder, attribute)
+            class_eval <<-RUBY
+              def #{finder}(#{attribute}) 
+                find(:#{attribute} => #{attribute})
+              end
+            RUBY
+          end
         
       end
     end
--- spec/cleansearch/cleansearch_spec.rb
+++ spec/cleansearch/cleansearch_spec.rb
@@ -1,14 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe 'dynamic find' do
+describe 'system class behavior'
   
   it 'should ensure that every object who inherit from activerecord has the association with CleanSearch::Search:MethodMissing' do
     User.should be_kind_of(CleanSearch::Search::MethodMissing)
   end
   
-  it 'should call the custom method_missing' do
-    User.should_receive(:find).with(:first_name => 'Foo')
-    User.find_first_name('Foo')
+end
+
+describe 'finder basic clauses' do
+  
+  before do
+    @match = Matcher.new(:find_first_name)
+  end
+  
+  it 'should have attribute :first_name' do
+    @match.attribute.should == :first_name
+  end
+
+  it 'should be a match' do
+    @match.should be_a_match
   end
   
 end
